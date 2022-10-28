1cc9e75097478ce02487cdca8caf3ce710ae9067
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2022-10-28 19:41:19 +0100

    finished project

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 
@@ -24,6 +25,7 @@ public class MainFrame extends JFrame {
 
         JPanel formPanel = new JPanel();
         formPanel.setLayout(new GridLayout(4, 1, 5,5));
+        formPanel.setOpaque(false);
         formPanel.add(lbFirstName);
         formPanel.add(tfFirstName);
         formPanel.add(lbLastName);
@@ -33,16 +35,58 @@ public class MainFrame extends JFrame {
         lbWelcome = new JLabel();
         lbWelcome.setFont(mainFont);
 
+        /*Button Panel*/
+        JButton btnOK = new JButton("OK");
+        btnOK.setFont(mainFont);
+        btnOK.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO 
+                String firstName = tfFirstName.getText();
+                String lastName = tfLastName.getText();
+                lbWelcome.setText("Hello " + firstName + " " + lastName);
+            }
+        });
+
+        JButton btnClear = new JButton("Clear");
+        btnClear.setFont(mainFont);
+        btnClear.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO 
+                tfFirstName.setText("");
+                tfLastName.setText("");
+                lbWelcome.setText("");
+            }
+        });
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(1,2,5,5));
+        buttonsPanel.setOpaque(false);
+        buttonsPanel.add(btnOK);
+        buttonsPanel.add(btnClear);
+
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
         mainPanel.setBackground(new Color(128, 128, 255));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         mainPanel.add(formPanel, BorderLayout.NORTH);
         mainPanel.add(lbWelcome, BorderLayout.CENTER);
-        
+        mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+
+        add(mainPanel);
+
         setTitle("Welcome");
         setSize(500, 600);
         setMinimumSize(new Dimension(300, 400));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainFrame myFrame = new MainFrame();
+        myFrame.initialize();
     }
 }
