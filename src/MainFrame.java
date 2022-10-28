e0fa1894acbeb9c03f2852033f17989c20767e98
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2022-10-28 18:52:55 +0100

    added first name and last name as a jpanel variables

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,7 +3,28 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+    final private Font mainFont = new Font("Segoe print", Font.BOLD, 18);
+    JTextField tfFirstName, tfLastName;
+
     public void initialize() {
+        /*Font Panel*/
+
+        JLabel lbFirstName = new JLabel("first name");
+        lbFirstName.setFont(mainFont);
+
+        tfFirstName = new JTextField();
+        tfFirstName.setFont(mainFont);
+
+        JLabel lbLastName = new JLabel("last name");
+        lbLastName.setFont(mainFont);
+
+        tfLastName = new JTextField();
+        tfLastName.setFont(mainFont);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(new Color(128, 128, 255));
+
         setTitle("Welcome");
         setSize(500, 600);
         setMinimumSize(new Dimension(300, 400));
