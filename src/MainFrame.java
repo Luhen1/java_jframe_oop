58d354b0c0858dfba58cc7033ec0c84e0779c30c
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2022-10-28 19:08:09 +0100

    Added welcome label

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class MainFrame extends JFrame {
     final private Font mainFont = new Font("Segoe print", Font.BOLD, 18);
     JTextField tfFirstName, tfLastName;
+    JLabel lbWelcome;
 
     public void initialize() {
         /*Font Panel*/
@@ -21,10 +22,23 @@ public class MainFrame extends JFrame {
         tfLastName = new JTextField();
         tfLastName.setFont(mainFont);
 
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(4, 1, 5,5));
+        formPanel.add(lbFirstName);
+        formPanel.add(tfFirstName);
+        formPanel.add(lbLastName);
+        formPanel.add(tfLastName);
+        
+        /*welcome panel */
+        lbWelcome = new JLabel();
+        lbWelcome.setFont(mainFont);
+
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
         mainPanel.setBackground(new Color(128, 128, 255));
-
+        mainPanel.add(formPanel, BorderLayout.NORTH);
+        mainPanel.add(lbWelcome, BorderLayout.CENTER);
+        
         setTitle("Welcome");
         setSize(500, 600);
         setMinimumSize(new Dimension(300, 400));
